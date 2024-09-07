1bc7fa32825c6fad8a40ca7163132415e05b0189
Lise Freja Jensen Petculescu <124042840+LisePetculescu@users.noreply.github.com>
2024-09-07 11:30:56 +0200

    found works

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const values = [21, 22, 23, 25, 27, 28, 29, 31, 32, 34, 35];
-const number = 25;
+const number = 28;
 let index = binarySearch(number, values);
 console.log(`Fundet tallet ${number} på index: ${index}`);
 
 function binarySearch(search, values) {
   // klar til kode!
   let min = 0;
-  let max = values.length;
+  let max = values.length - 1;
   let middle = Math.floor((max + min) / 2);
   let found = false;
   console.log(`min: ${min} and max: ${max}`);
   console.log(`middle: ${middle}`);
 
-  while (min <= max) {
+  while (min <= max && !found) {
     middle = Math.floor((min + max) / 2);
     console.log(`min: ${min}, max: ${max}, middle: ${middle}`);
 
@@ -23,6 +23,7 @@ function binarySearch(search, values) {
     } else if (comparison < 0) {
       min = middle + 1;
     } else if (comparison == 0) {
+      found = true;
       return middle;
     }
   }
